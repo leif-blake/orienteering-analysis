74d14c4296db0b570a20155d85a13fd565aabd09
leif-blake <76570647+leif-blake@users.noreply.github.com>
2024-08-30 18:57:59 -0700

    Improve efficiency of split performance
    Splits dictionary is now a splits dataframe. Improved efficiency of splits import and split performance processing. Can now normalize splits within class only

--- plotter.py
+++ plotter.py
@@ -9,14 +9,17 @@ import matplotlib.dates as mdates
 
 import utilities
 
-def plot_split_performances(df: pd.DataFrame, race_name, tz, normalized=True, alpha=0.2, y_min=None, y_max=None,
+
+def plot_split_performances(df: pd.DataFrame, race_name, tz, normalize_to_individual=False, normalize_to_class=True,
+                            alpha=0.2, y_min=None, y_max=None,
                             average_window=300, time_step=30, use_start_time=False):
     """
     Plots split performance against time (normalized or not)
     :param df: Split performances DataFrame
     :param race_name: Name of the race
     :param tz: Timezone of the race
-    :param normalized: Whether to normalize the performances for an individual athlete or not
+    :param normalize_to_individual: Whether to normalize the split performance for an individual athlete or not
+    :param normalize_to_class: Whether to normalize the split performance to the class or overall
     :param alpha: Alpha parameter for plotting individual split performance
     :param y_min: Minimum y value for y axis
     :param y_max: Maximum y value for y axis
@@ -25,12 +28,14 @@ def plot_split_performances(df: pd.DataFrame, race_name, tz, normalized=True, al
     :return:
     """
 
-    perf_column = 'normalized_performance' if normalized else 'performance'
+    # Build columns
+    perf_column = 'norm_' if normalize_to_individual else ''
+    perf_column += 'class_perf' if normalize_to_class else 'overall_perf'
     time_column = 'start_time' if use_start_time else 'timestamp'
 
     # Get average line
     averages_df = utilities.window_avg_line(df, perf_column, time_column=time_column,
-                                  average_window=average_window, time_step=time_step)
+                                            average_window=average_window, time_step=time_step)
 
     # Convert Unix timestamps to datetime objects, adjust for time zone difference
     tz_local_offset = utilities.get_offset_from_local(tz)
@@ -48,7 +53,7 @@ def plot_split_performances(df: pd.DataFrame, race_name, tz, normalized=True, al
     plt.xlabel(f'{"Athlete Start Time" if use_start_time else "Time"} (hh:mm:ss)')
     plt.ylabel('Relative Performance (lower is better)')
     plt.title(f'{race_name}\n'
-              f'{"Normalized " if normalized else ""}Split Performance vs {"Athlete Start " if use_start_time else ""}Time\n'
+              f'{"Normalized " if normalize_to_individual else ""}Split Performance vs {"Athlete Start " if use_start_time else ""}Time\n'
               f'Line is moving average with {average_window / 60} minute width')
 
     plt.grid(True)
--- split_time_analysis.py
+++ split_time_analysis.py
@@ -7,138 +7,128 @@ import sqlite3
 from tkinter import filedialog
 import numpy as np
 import pandas as pd
-import random
+import time
 
 import plotter
 import utilities
 
 
-def import_all_splits(db_filename: str, race_id: str, class_list: list[str]=None):
+def import_all_splits(db_filename: str, race_id: str, class_list: list[str] = None):
     """
     Imports all split times between two consecutive controls for a given race_id
     :param db_filename: Path to the database file
     :param race_id: Race ID from database
     :param class_list: List of classes to import. If empty, import all classes
-    :return: Dictionary with all legs and splits times, as well as standard deviation
+    :return: Dataframe with all legs and splits times
     """
+
+    func_start_time = time.time()
 
     conn = sqlite3.connect(db_filename)
     cursor = conn.cursor()
 
+    query_start_time = time.time()
+
     if class_list is not None:
-        query = (f'SELECT controls, control_times FROM results INNER JOIN races_competitors '
-                 f'ON results.card_no = races_competitors.card_no AND results.race_id = races_competitors.race_id '
-                 f'WHERE results.race_id = ? AND class_id IN ({",".join(["?"]*len(class_list))})')
+        query = (
+            f'SELECT start_time, controls, control_times, competitor_id, class_id FROM results INNER JOIN races_competitors '
+            f'ON results.card_no = races_competitors.card_no AND results.race_id = races_competitors.race_id '
+            f'WHERE results.race_id = ? AND class_id IN ({",".join(["?"] * len(class_list))})')
         values = [race_id] + class_list
         cursor.execute(query, values)
     else:
-        cursor.execute('SELECT controls, control_times FROM results WHERE race_id = ?', (race_id,))
+        query = query = (
+            f'SELECT start_time, controls, control_times, competitor_id, class_id FROM results INNER JOIN races_competitors '
+            f'ON results.card_no = races_competitors.card_no AND results.race_id = races_competitors.race_id '
+            f'WHERE results.race_id = ?')
+        cursor.execute(query, (race_id,))
     rows = cursor.fetchall()
 
-    dict = {}
+    conn.close()
+
+    query_end_time = time.time()
+    print('Time to run split import query: ' + str(query_end_time - query_start_time))
+    print(f'Fetched {len(rows)} rows')
+
+    splits_dict_list = []
+
     for row in rows:
-        controls = ast.literal_eval(row[0])
-        control_times = ast.literal_eval(row[1])
+        start_time = row[0]
+        controls = ast.literal_eval(row[1])
+        control_times = ast.literal_eval(row[2])
+        competitor_id = row[3]
+        class_id = row[4]
 
         for i in range(len(controls) - 1):
             control_sequence = f'{controls[i]}-{controls[i + 1]}'
-            if control_sequence not in dict:
-                dict[control_sequence] = {}
-                dict[control_sequence]['times'] = []
             try:
-                dict[control_sequence]['times'].append(control_times[i + 1] - control_times[i])
+                splits_dict = {'ctrl_seq': control_sequence,
+                               'timestamp': control_times[i],
+                               'start_time': start_time,
+                               'competitor_id': competitor_id,
+                               'class_id': class_id,
+                               'split_time': control_times[i + 1] - control_times[i],}
+                splits_dict_list.append(splits_dict)
             except TypeError:
                 continue  # Some of the control times are NULL for mispunches
 
-    sequences_to_remove = []
-    for control_sequence, sub_dict in dict.items():
-        # Remove control times if empty
-        if len(sub_dict['times']) == 0:
-            sequences_to_remove.append(control_sequence)
-            continue
-        sub_dict['avg'] = np.average(sub_dict['times'])
-        sub_dict['dev'] = np.std(sub_dict['times'])
+    splits_df = pd.DataFrame(splits_dict_list)
 
-    for control_sequence in sequences_to_remove:
-        del dict[control_sequence]
+    func_end_time = time.time()
+    print('Total time to import splits: ' + str(func_end_time - func_start_time))
 
-    conn.close()
-
-    return dict
+    return splits_df
 
 
-def calc_split_performance(start_time, controls, control_times, splits_dict):
+def calc_split_performances(splits_df, min_start_time=0, class_list: list[str] = None):
     """
-    Calculates the per-split performance of a given competitor, relative to their average split performance
-    :param start_time: Start time of the competitor
-    :param controls: List of controls
-    :param control_times: List of control times
-    :param splits_dict: Imported dictionary of all split times
-    :return: DataFrame with performances, normalized performances, and timestamps
-    """
-
-    # Calculate split performance of competitor relative to all other competitors
-    split_performances = pd.DataFrame(columns=['start_time', 'timestamp', 'performance'])
-    for i in range(len(controls) - 1):
-        control_sequence = f'{controls[i]}-{controls[i + 1]}'
-        try:
-            split_time = control_times[i + 1] - control_times[i]
-        except TypeError:
-            continue  # Some of the control times are NULL for mispunches
-
-        split_timestamp = (control_times[i] + control_times[i + 1]) / 2
-        split_performance = split_time / splits_dict[control_sequence]['avg']
-        split_performances.loc[len(split_performances)] = [start_time, split_timestamp, split_performance]
-
-    # Return empty if no valid splits were found
-    if split_performances.empty:
-        return split_performances
-
-    split_performances['normalized_performance'] = split_performances['performance'] / np.mean(
-        split_performances['performance'])
-
-    return split_performances
-
-
-def get_all_split_performances(db_filename, race_id, splits_dict, min_start_time=0, class_list: list[str]=None):
-    """
-    Gets normalized split performances for all competitors, removing reference to the competitors and control sequences
+    Gets normalized split performances for all competitors from a splits dataframe
     :param db_filename: Database file path
     :param race_id: Race ID from database
-    :param splits_dict: Dictionary with all split times
+    :param splits_df: Dictionary with all split times
     :param min_start_time: Minimum start time of competitors
     :param class_list: List of classes to import. If empty, import all classes
+    :param normalize_to_class: Normalize split performances to average within class, not across all classes
     :return: DataFrame with performances, normalized performances, and timestamps. Also return random classes
     """
 
-    conn = sqlite3.connect(db_filename)
-    cursor = conn.cursor()
+    func_start_time = time.time()
 
+    split_perf_columns = ['ctrl_seq', 'timestamp', 'start_time', 'class_id', 'class_perf', 'overall_perf', 'norm_class_perf',
+                          'norm_overall_perf']
+
+    split_perf_df = pd.DataFrame(columns=split_perf_columns)
+
+    # Masks
+    start_time_mask = (splits_df['start_time'] != None) & (splits_df['start_time'] % 86400 >= min_start_time)
     if class_list is not None:
-        query = (f'SELECT start_time, controls, control_times FROM results INNER JOIN races_competitors '
-                 f'ON results.card_no = races_competitors.card_no AND results.race_id = races_competitors.race_id '
-                 f'WHERE results.race_id = ? AND class_id IN ({",".join(["?"] * len(class_list))})')
-        values = [race_id] + class_list
-        cursor.execute(query, values)
+        class_mask = splits_df['class_id'].isin(class_list)
     else:
-        cursor.execute('SELECT start_time, controls, control_times FROM results WHERE race_id = ?', (race_id,))
+        class_mask = np.ones_like(splits_df['class_id'][start_time_mask], dtype=bool)
 
-    split_performances = pd.DataFrame(columns=['start_time', 'timestamp', 'performance', 'normalized_performance'])
-    for row in cursor.fetchall():
-        # Check for minimum start time requirement (in UTC)
-        if row[0] is None or int(row[0]) % 86400 < min_start_time:
-            continue
-        df = calc_split_performance(int(row[0]), ast.literal_eval(row[1]), ast.literal_eval(row[2]), splits_dict)
-        if split_performances.empty:
-            split_performances = df.copy()
-        elif not df.empty:
-            split_performances = pd.concat([split_performances, df], ignore_index=True)
+    # Copy over existing columns from splits df, passing through mask
+    split_perf_df['ctrl_seq'] = splits_df['ctrl_seq'][start_time_mask][class_mask]
+    split_perf_df['class_id'] = splits_df['class_id'][start_time_mask][class_mask]
+    split_perf_df['timestamp'] = splits_df['timestamp'][start_time_mask][class_mask]
+    split_perf_df['start_time'] = splits_df['start_time'][start_time_mask][class_mask]
+    split_perf_df['competitor_id'] = splits_df['competitor_id'][start_time_mask][class_mask]
 
-    split_performances.reset_index(inplace=True)
+    # Calculate performance by class and overall
+    split_perf_df['class_perf'] = splits_df.groupby(['ctrl_seq', 'class_id'])['split_time'].transform(
+        lambda x: (x / x.mean()))
+    split_perf_df['overall_perf'] = splits_df.groupby('ctrl_seq')['split_time'].transform(
+        lambda x: (x / x.mean()))
 
-    conn.close()
+    # Calculate performance normalized to the individual
+    split_perf_df['norm_class_perf'] = split_perf_df.groupby('competitor_id')['class_perf'].transform(
+        lambda x: (x / x.mean()))
+    split_perf_df['norm_overall_perf'] = split_perf_df.groupby('competitor_id')['overall_perf'].transform(
+        lambda x: (x / x.mean()))
 
-    return split_performances
+    func_end_time = time.time()
+    print('Time to calculate all split performances: ' + str(func_end_time - func_start_time))
+
+    return split_perf_df
 
 
 if __name__ == '__main__':
@@ -155,8 +145,8 @@ if __name__ == '__main__':
     conn = sqlite3.connect(db_file_path)
     cursor = conn.cursor()
 
-    min_start_time = 6 * 3600  # To remove competitors given artificial start times of midnight
-    average_window = 60*30
+    min_start_time = 7 * 3600  # To remove competitors given artificial start times of midnight
+    average_window = 60 * 30
     y_min_zoom = 0.6
     y_max_zoom = 1.4
     use_start_time = True
@@ -169,22 +159,18 @@ if __name__ == '__main__':
         class_list = None
 
     # race_ids = [1, 2, 3, 4]
-    race_ids = [1]
+    race_ids = [4]
     # race_ids = [3]
     for race_id in race_ids:
         cursor.execute('SELECT name FROM races WHERE id = ?', (race_id,))
         race_name = cursor.fetchone()[0]
-        splits_dict = import_all_splits(db_file_path, race_id, class_list=class_list)
-        split_performances = get_all_split_performances(db_file_path, race_id, splits_dict,
-                                                        min_start_time=min_start_time,
-                                                        class_list=class_list)
+        splits_df = import_all_splits(db_file_path, race_id, class_list=class_list)
+        split_performances = calc_split_performances(splits_df, min_start_time=min_start_time,
+                                                     class_list=class_list)
         plotter.plot_split_performances(split_performances, race_name, tz, average_window=average_window,
                                 use_start_time=use_start_time)
-        plotter.plot_split_performances(split_performances, race_name, tz, y_min=y_min_zoom, y_max=y_max_zoom,
-                                average_window=average_window, use_start_time=use_start_time)
-        plotter.plot_split_performances(split_performances, race_name, tz, normalized=False, average_window=average_window,
-                                use_start_time=use_start_time)
-        plotter.plot_split_performances(split_performances, race_name, tz, normalized=False, y_min=y_min_zoom, y_max=y_max_zoom,
-                                average_window=average_window, use_start_time=use_start_time)
+        plotter.plot_split_performances(split_performances, race_name, tz, y_min=y_min_zoom,
+                                        y_max=y_max_zoom,
+                                        average_window=average_window, use_start_time=use_start_time)
 
     print("Done")
